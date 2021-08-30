d8374c9eaa7284daa82a6dbec2e0b1f84f67b2a8
liushuang <liushuang@jilinxiangyun.com>
2021-08-30 11:50:10 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,12 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
-  export default defineComponent({
-    name: "App",
-    components: {},
-  });
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
 </script>
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
+<style></style>

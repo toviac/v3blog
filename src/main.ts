de71ece9213e9bcf4f9b4f0feb3a7288ace63215
lilei <1045606768@qq.com>
2021-09-01 13:47:08 +0800

    add el-loading

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-
 import axios from '@/plugins/axios';
+import { ElLoading } from 'element-plus';
 
 import '@/style/common.scss';
 
 const app = createApp(App);
+ElLoading.install(app);
 app.config.globalProperties.$axios = axios;
 app.use(router);
 app.mount('#app');

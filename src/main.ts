7c9156363f27b555697800c87a26ed3130fded48
lilei <1045606768@qq.com>
2021-10-11 11:15:43 +0800

    add elmessage

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import router from './router';
 import store from './store';
 import '@/utils/disableLog';
 import axios from '@/plugins/axios';
-import { ElLoading } from 'element-plus';
+import { ElLoading, ElMessage } from 'element-plus';
 import 'element-plus/dist/index.css';
 
 import '@/style/common.scss';
 
 const app = createApp(App);
 ElLoading.install(app);
+app.config.globalProperties.$message = ElMessage;
 app.config.globalProperties.$axios = axios;
 app.use(router);
 app.use(store);
